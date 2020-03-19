9c55d8a7d481a6d99b332e96d4a167da03349157
Dmitri Maksimov <dmitri.maksimov@nortal.com>
2020-03-19 17:34:17 +0200

    Added default headers to OpenAPI config.

--- src/main/java/eu/maksimov/demo/spring/versioning/config/OpenApiConfig.java
+++ src/main/java/eu/maksimov/demo/spring/versioning/config/OpenApiConfig.java
@@ -1,8 +1,13 @@
 package eu.maksimov.demo.spring.versioning.config;
 
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +18,10 @@ import org.springframework.core.io.ResourceLoader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.joining;
 
 @Configuration
@@ -38,6 +46,12 @@ public class OpenApiConfig {
       .externalDocs(new ExternalDocumentation()
         .description("GitHub repository")
         .url("https://github.com/mcdimus/demo-spring-api-versioning-and-swagger")
+      )
+      .components(new Components()
+        .addParameters("header-x-application-id",
+          new Parameter().in(ParameterIn.HEADER.toString()).name("X-Application-Id").required(true).schema(new StringSchema()._default("swagger-ui")))
+        .addParameters("header-x-user-id",
+          new Parameter().in(ParameterIn.HEADER.toString()).name("X-User-Id").required(true).schema(new StringSchema()._default("swagger-ui")))
       );
   }
 
@@ -49,6 +63,19 @@ public class OpenApiConfig {
     ) {
       return reader.lines().collect(joining("\n"));
     }
+  }
+
+  @Bean
+  public OperationCustomizer operationCustomizer() {
+    return (operation, handlerMethod) -> {
+      List<Parameter> newParameters = new ArrayList<>();
+      newParameters.add(new Parameter().$ref("#/components/parameters/header-x-application-id"));
+      newParameters.add(new Parameter().$ref("#/components/parameters/header-x-user-id"));
+      newParameters.addAll(operation.getParameters() == null ? emptyList() : operation.getParameters());
+
+      operation.setParameters(newParameters);
+      return operation;
+    };
   }
 
 }
