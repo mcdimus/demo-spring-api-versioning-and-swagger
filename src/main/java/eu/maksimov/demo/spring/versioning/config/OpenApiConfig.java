374d175ece9308673005fba39a9a48f608dc3dbb
Dmitri Maksimov <dmitri.maksimov@nortal.com>
2020-03-20 19:15:34 +0200

    Added security scheme definition.

--- src/main/java/eu/maksimov/demo/spring/versioning/config/OpenApiConfig.java
+++ src/main/java/eu/maksimov/demo/spring/versioning/config/OpenApiConfig.java
@@ -7,20 +7,25 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
+import static io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP;
 import static java.util.Collections.emptyList;
 import static java.util.stream.Collectors.joining;
 
@@ -49,17 +54,20 @@ public class OpenApiConfig {
       )
       .components(new Components()
         .addParameters("header-x-application-id",
-          new Parameter().in(ParameterIn.HEADER.toString()).name("X-Application-Id").required(true).schema(new StringSchema()._default("swagger-ui")))
-        .addParameters("header-x-user-id",
-          new Parameter().in(ParameterIn.HEADER.toString()).name("X-User-Id").required(true).schema(new StringSchema()._default("swagger-ui")))
+          new Parameter().in(ParameterIn.HEADER.toString()).name("X-Application-Id").required(true).schema(new StringSchema()._default("swagger-ui"))
+        ).addParameters("header-x-user-id",
+          new Parameter().in(ParameterIn.HEADER.toString()).name("X-User-Id").required(true).schema(new StringSchema()._default("swagger-ui"))
+        ).addSecuritySchemes("access-token",
+          new SecurityScheme().type(HTTP).scheme("bearer").in(SecurityScheme.In.HEADER).name("Authorization")
+        )
       );
   }
 
   private String getResourceContents(String location) throws IOException {
-    Resource resource = resourceLoader.getResource(location);
+    var resource = resourceLoader.getResource(location);
     try (
-      InputStreamReader in = new InputStreamReader(resource.getInputStream());
-      BufferedReader reader = new BufferedReader(in)
+      var in = new InputStreamReader(resource.getInputStream());
+      var reader = new BufferedReader(in)
     ) {
       return reader.lines().collect(joining("\n"));
     }
@@ -72,8 +80,13 @@ public class OpenApiConfig {
       newParameters.add(new Parameter().$ref("#/components/parameters/header-x-application-id"));
       newParameters.add(new Parameter().$ref("#/components/parameters/header-x-user-id"));
       newParameters.addAll(operation.getParameters() == null ? emptyList() : operation.getParameters());
-
       operation.setParameters(newParameters);
+
+      Stream<String> paths = Arrays.stream(handlerMethod.getBeanType().getAnnotation(RequestMapping.class).value());
+      if (paths.allMatch(it -> it.matches("/api/v\\d{1,2}/session"))) {
+        operation.addSecurityItem(new SecurityRequirement().addList("access-token"));
+      }
+
       return operation;
     };
   }
